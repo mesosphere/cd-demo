298a0cccf0f0db576653959a57f4ae31208a182e
Scott Schneider <scott@mesosphere.com>
2017-08-07 15:02:49 -0700

    Patch to work with 1.10 (dcoscli `0.5.3`)

--- bin/demo.py
+++ bin/demo.py
@@ -116,8 +116,17 @@ def check_and_set_token(dcos_url):
                 'did you provide --dcos-username and --dcos-password or --dcos-oauth-token?')
 
 def config_dcos_cli(dcos_url):
-    dcos.config.set_val('core.dcos_url', dcos_url)
-    dcos.config.set_val('core.ssl_verify', 'False')
+    with dcos.cluster.setup_directory() as temp_path:
+        dcos.cluster.set_attached(temp_path)
+        dcos.config.set_val('core.dcos_url', dcos_url)
+        dcos.config.set_val('core.ssl_verify', 'False')
+
+        try:
+            shakedown.dcos_leader()
+        except:
+            check_and_set_token(dcos_url)
+            dcos.cluster.setup_cluster_config(dcos_url, temp_path, False)
+
 
 def install_jenkins(jenkins_name, jenkins_url):
     log("installing Jenkins with name '{}'".format(jenkins_name))
@@ -381,7 +390,6 @@ if __name__ == "__main__":
         jenkins_version = None
 
     config_dcos_cli(dcos_url)
-    check_and_set_token(dcos_url)
 
     try:
         if arguments['install']:
