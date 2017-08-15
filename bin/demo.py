b0aac8e40dbe2980eb7d080aef462e0bdc374af0
Scott Schneider <scott@mesosphere.com>
2017-08-15 12:49:44 -0700

    Update `marathon-lb` package to `1.10.0` (latest)
    Update the deployed app's marathon payload for `1.10.0` as well.

--- bin/demo.py
+++ bin/demo.py
@@ -45,7 +45,7 @@ from urllib.parse import urlparse
 from shakedown import *
 
 jenkins_version = "3.2.2-2.60.2"
-marathon_lb_version="1.3.5"
+marathon_lb_version="1.10.0"
 
 def log(message):
     print("[demo]: {}".format(message))
